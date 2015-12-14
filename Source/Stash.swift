bec7ae850e4a154bd620246c7ceefb77465783ab
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-14 15:48:05 +0100

    Add skeleton

--- Source/Stash.swift
+++ Source/Stash.swift
@@ -1,4 +1,21 @@
 import Foundation
 
 struct Stash {
+
+  static func password(service: String, account: String) -> String {
+
+    return ""
+  }
+
+  static func setPassword(password: String, service: String, account: String) {
+
+  }
+
+  static func setPasswordData(password: NSData, service: String, account: String) {
+    
+  }
+
+  static func delete(service: String, account: String) {
+
+  }
 }
