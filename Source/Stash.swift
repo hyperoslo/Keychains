03164d8d6287646e94e5d468c89791298657bc96
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-14 20:26:07 +0100

    Add Stash implementation

--- Source/Stash.swift
+++ Source/Stash.swift
@@ -1,21 +1,87 @@
 import Foundation
+import Security
 
-struct Stash {
+public struct Stash {
 
-  static func password(service: String, account: String) -> String {
-
-    return ""
+  enum Action {
+    case Insert, Fetch, Delete
   }
 
-  static func setPassword(password: String, service: String, account: String) {
+  // MARK: - Public methods
 
+  public static func password(service: String, account: String) -> String {
+    guard !service.isEmpty && !account.isEmpty else { return "" }
+
+    var query = [String : AnyObject]()
+    query[kSecClass as String] = kSecClassGenericPassword
+    query[kSecAttrService as String] = service
+    query[kSecAttrAccount as String] = account
+
+    return Stash.query(.Fetch, query: query).1
   }
 
-  static func setPasswordData(password: NSData, service: String, account: String) {
-    
+  public static func setPassword(password: String, service: String, account: String) -> Bool {
+    guard !service.isEmpty && !account.isEmpty else { return false }
+
+    var query = [String : AnyObject]()
+    query[kSecClass as String] = kSecClassGenericPassword
+    query[kSecAttrService as String] = service
+    query[kSecAttrAccount as String] = account
+    query[kSecAttrAccessible as String] = kSecAttrAccessibleWhenUnlocked
+
+    return Stash.query(.Insert, query: query, password: password).0 == errSecSuccess
   }
 
-  static func delete(service: String, account: String) {
+  public static func delete(service: String, account: String) -> Bool {
+    guard !service.isEmpty && !account.isEmpty else { return false }
 
+    var query = [String : AnyObject]()
+    query[kSecClass as String] = kSecClassGenericPassword
+    query[kSecAttrService as String] = service
+    query[kSecAttrAccount as String] = account
+
+    return Stash.query(.Delete, query: query).0 == errSecSuccess
+  }
+
+  // MARK: - Private methods
+
+  private static func query(action: Action, query: [String : AnyObject], password: String = "") -> (OSStatus, String) {
+    let passwordData = password.dataUsingEncoding(NSUTF8StringEncoding)
+    var returnPassword = ""
+    var status = SecItemCopyMatching(query, nil)
+    var attributes = [String : AnyObject]()
+
+    switch action {
+    case .Insert:
+      switch status {
+      case errSecSuccess:
+        attributes[kSecValueData as String] = passwordData
+        status = SecItemUpdate(query as CFDictionaryRef, attributes)
+        break
+      case errSecItemNotFound:
+        var query = query
+        query[kSecValueData as String] = passwordData
+        status = SecItemAdd(query as CFDictionaryRef, nil)
+        break
+      default: break
+      }
+    case .Fetch:
+      var query = query
+      query[kSecReturnData as String] = true
+      query[kSecMatchLimit as String] = kSecMatchLimitOne
+
+      var result: CFTypeRef?
+      status = SecItemCopyMatching(query, &result)
+
+      if let result = result as? NSData,
+        password = String(data: result, encoding: NSUTF8StringEncoding) {
+          returnPassword = password
+      }
+      break
+    case .Delete:
+      status = SecItemDelete(query)
+    }
+
+    return (status, returnPassword)
   }
 }
